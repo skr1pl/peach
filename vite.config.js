1c69051655a798c8e8eada92b1e6884ba10bdd7d
skr1pl <dmitriy.rusov@bk.ru>
2025-04-06 00:27:49 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
     root: 'src',
-    base: '/peach-project/',
+    base: '/peach/',
     build: {
         outDir: '../dist',
         emptyOutDir: true,
